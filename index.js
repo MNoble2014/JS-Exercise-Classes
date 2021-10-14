c495eb6a5143ac9af5f33e1f8a8f14d247bc9c67
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-14 19:14:20 -0400

    Task 4 updated

--- index.js
+++ index.js
@@ -140,21 +140,20 @@ console.log('Task 3');
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 class Instructor extends Lambdasian {
-  constructor({name, age, location, specialty, favLanguage, catchPhrases}) {
-    super({name, age, location, specialty, favLanguage, catchPhrases});
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}) {
+    super({name, age, location, specialty, favLanguage, catchPhrase});
     this.specialty = specialty;
     this.favLanguage = favLanguage;
     this.catchPhrase = catchPhrase;
   }
-  demo (subject) {
+  demo(subject) {
     return `Today we are learning about ${subject}`;
   }
-  grade (student, subject) {
+  grade(student, subject) {
     return `${student.name} receives a perfect score on ${subject}`;
   }
-};
-
-console.log('Task 4');
+}
+  
 
 /*
   TASK 5
