57f9c56c3f7c7ba90d00e50417013739fc8e6f2f
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-14 17:30:49 -0400

    Task 2 test passing

--- index.js
+++ index.js
@@ -82,8 +82,28 @@ console.log(john.stomach);
 */
 
 class Car {
-  
+  constructor (model, mpg) {
+    this.model = model;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 }
+Car.prototype.drive = function(gallons) {
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(dist) {
+  const driveableMiles = this.tank - this.milesPerGallon;
+  if(dist <= driveableMiles) {
+    this.odometer = this.odometer + dist;
+    this.tank = this.tank - (dist / this.milePerGallon);
+  } else {
+    this.odometer = this.odometer + driveableMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
+}
+console.log('Task 2');
 
 /*
   TASK 3
